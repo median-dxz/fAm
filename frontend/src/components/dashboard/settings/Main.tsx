726cbd99cfbc38abcb9e5642649c5b87f236215e
median-dxz <med_iridescence@163.com>
2024-04-03 14:07:20 +0800

    script: 更新容器配置

--- frontend/src/components/dashboard/settings/Main.tsx
+++ frontend/src/components/dashboard/settings/Main.tsx
@@ -26,7 +26,7 @@ function StrategyServiceSettings({ setting }: StrategyServiceSettingsProps) {
   const [strategyService, setStrategyService] = useState(setting);
   const [updating, setUpdating] = useState(false);
   const [dialogOpen, setDialogOpen] = useState(false);
-  const [dialogStatus, setDialogStatus] = useState({ message: "", success: false });
+  const [dialogStatus, setDialogStatus] = useState({ title: "", message: "", success: false });
 
   return (
     <Card className="flex flex-col space-y-4">
@@ -43,19 +43,38 @@ function StrategyServiceSettings({ setting }: StrategyServiceSettingsProps) {
             setUpdating(true);
             const r = await updateStratyegyService({ body: { ...strategyService, url: v } });
             setUpdating(false);
-            setDialogStatus(r);
+            if (r.success === false) {
+              setDialogStatus({ ...r, title: "更新失败" });
+              setDialogOpen(true);
+            }
           }}
         />
       </label>
       <div className="flex flex-row space-x-2">
-        <Button onClick={async () => {}}>测试</Button>
+        <Button
+          onClick={async () => {
+            try {
+              setUpdating(true);
+              await fetch(strategyService.url); // TODO test on server side
+              // 更改message
+              setDialogStatus({ title: "测试策略服务连接", message: "测试成功", success: true });
+              setDialogOpen(true);
+            } catch (error) {
+              setDialogStatus({ title: "测试策略服务连接", message: (error as Error).message, success: false });
+              setDialogOpen(true);
+            } finally {
+              setUpdating(false);
+            }
+          }}
+        >
+          测试
+        </Button>
       </div>
 
       <Dialog open={dialogOpen} onClose={() => setDialogOpen(false)} static={true} className="z-[100]">
         <DialogPanel className="max-w-sm">
-          <p className="text-tremor-title text-tremor-content-strong font-bold mb-4">
-            {dialogStatus.success ? "更新成功" : "更新失败"}
-          </p>
+          <p className="text-tremor-title text-tremor-content-strong font-bold mb-4">{dialogStatus.title}</p>
+          <p className="text-tremor-content-emphasis mb-4">操作{dialogStatus.success ? "成功" : "失败"}</p>
           <p className="text-tremor-content-emphasis mb-4">{dialogStatus.message}</p>
           <Button variant="light" className="mx-auto flex items-center" onClick={() => setDialogOpen(false)}>
             关闭
